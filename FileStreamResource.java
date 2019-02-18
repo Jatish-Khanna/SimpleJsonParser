1899de3743f42ae3ffc10cc632156e29d9efee59
Jatish Khanna <jatish.khanna@gmail.com>
2019-02-19 01:15:52 +0530

    Create FileStreamResource.java

---
+++ FileStreamResource.java
@@ -0,0 +1,34 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class FileStreamResource implements Resource {
+	String resourceLocation;
+	InputStreamReader streamReader;
+	Logger logger = Logger.getLogger(this.getClass().getName());
+
+	FileStreamResource(String resourceLocation) {
+		this.resourceLocation = resourceLocation;
+	}
+
+	@Override
+	public Optional<InputStreamReader> streamedResource() {
+		try {
+			FileInputStream fisReader = new FileInputStream(resourceLocation);
+			streamReader = new InputStreamReader(fisReader);
+		} catch (IOException e) {
+			logger.log(Level.SEVERE, e.getMessage(), e);
+		}
+
+		return Optional.ofNullable(streamReader);
+	}
+
+	@Override
+	public void close() throws Exception {
+		streamReader.close();
+	}
+}
